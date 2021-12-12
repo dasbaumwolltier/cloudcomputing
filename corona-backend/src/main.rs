7ac29881e8c2ac1f308647c63f396a2dea6b6cb8
dasbaumwolltier <gabriel@guldner.eu>
2021-12-12 17:32:19 +0100

    Add dockerfile and logs

--- corona-backend/src/main.rs
+++ corona-backend/src/main.rs
@@ -1,9 +1,20 @@
 use actix_web::{HttpServer, App};
+use log::{info, LevelFilter};
+use simplelog::{SimpleLogger, Config, TermLogger, TerminalMode, ColorChoice};
 
 pub mod handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    TermLogger::init(
+        LevelFilter::Debug,
+        Config::default(), 
+        TerminalMode::Stdout, 
+        ColorChoice::Auto
+    ).expect("Could not initialise logger!");
+
+    info!("Starting server!");
+
     HttpServer::new(|| 
         App::new().service(handlers::index)
     ).bind("0.0.0.0:8080")?
