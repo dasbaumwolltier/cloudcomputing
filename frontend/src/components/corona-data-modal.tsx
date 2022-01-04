f3e022266e71b83219fa3b4ef2c34e34c3095cc5
alexkainzinger <alexander@kainzinger.dev>
2022-01-04 20:59:52 +0100

    fix: show no data available text if no data was returned (frontend)

--- frontend/src/components/corona-data-modal.tsx
+++ frontend/src/components/corona-data-modal.tsx
@@ -160,64 +160,70 @@ const CoronaDataModal: React.FC<ICoronaDataModal> = ({ open, setOpen }) => {
 
                     <div className="border-t border-gray-200 pt-5">
                       {isLoadingCorona && <Spinner />}
-                      {!isLoadingCorona && coronaData.length ? (
+                      {!isLoadingCorona ? (
                         <>
-                          <Line options={options} data={chartData()} />
-                          <div className="flex flex-col mt-4">
-                            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                  <table className="min-w-full divide-y divide-gray-200">
-                                    <thead className="bg-gray-50 table table-fixed w-full">
-                                      <tr>
-                                        <th
-                                          scope="col"
-                                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Date
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Active
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Deaths
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Confirmed
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Recovered
-                                        </th>
-                                      </tr>
-                                    </thead>
-                                    <tbody className="overflow-hidden block">
-                                      <List
-                                        height={260}
-                                        itemCount={coronaData.length}
-                                        itemData={itemData.coronaData}
-                                        itemSize={52}
-                                        width="100%"
-                                      >
-                                        {TableEntry}
-                                      </List>
-                                    </tbody>
-                                  </table>
+                          {coronaData.length ? (
+                            <>
+                              <Line options={options} data={chartData()} />
+                              <div className="flex flex-col mt-4">
+                                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                  <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                      <table className="min-w-full divide-y divide-gray-200">
+                                        <thead className="bg-gray-50 table table-fixed w-full">
+                                          <tr>
+                                            <th
+                                              scope="col"
+                                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                              Date
+                                            </th>
+                                            <th
+                                              scope="col"
+                                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                              Active
+                                            </th>
+                                            <th
+                                              scope="col"
+                                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                              Deaths
+                                            </th>
+                                            <th
+                                              scope="col"
+                                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                              Confirmed
+                                            </th>
+                                            <th
+                                              scope="col"
+                                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                              Recovered
+                                            </th>
+                                          </tr>
+                                        </thead>
+                                        <tbody className="overflow-hidden block">
+                                          <List
+                                            height={260}
+                                            itemCount={coronaData.length}
+                                            itemData={itemData.coronaData}
+                                            itemSize={52}
+                                            width="100%"
+                                          >
+                                            {TableEntry}
+                                          </List>
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                  </div>
                                 </div>
                               </div>
-                            </div>
-                          </div>
+                            </>
+                          ) : (
+                            <div className="text-center text-gray-800">No data available</div>
+                          )}
                         </>
                       ) : null}
                     </div>
