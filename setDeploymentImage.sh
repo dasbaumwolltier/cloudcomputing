1823215dd2bb613c46b1a2f4990c274bbbeafc61
SchwapTobi <tobias.schwap@gmail.com>
2022-01-16 15:10:09 +0100

    setDeploymentImage.sh

--- setDeploymentImage.sh
+++ setDeploymentImage.sh
@@ -4,15 +4,15 @@
 SERVICE="$1"	# frontend | corona-backend | country-backend
 TAG_NAME="$2"	# e.d registry.guldner.eu/frontend:20220116-1320
 
-if [[ -z "$SERVICE" ]]; then
-	printf "SERVICE is not provided"
-	exit 1
-fi
+#if [[ -z "$SERVICE" ]]; then
+#	printf "SERVICE is not provided"
+#	exit 1
+#fi
 
-if [[ -z "$TAG_NAME" ]]; then
-	printf "TAG_NAME is not provided"
-	exit 1
-fi
+#if [[ -z "$TAG_NAME" ]]; then
+#	printf "TAG_NAME is not provided"
+#	exit 1
+#fi
 
 DEPLOYMENT_FILE="./cluster/$SERVICE/resources/deployment.yaml"
 
