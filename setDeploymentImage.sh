1556bca9a8bbc6ff39da2b4479d8c5f1237a4632
SchwapTobi <tobias.schwap@gmail.com>
2022-01-16 15:52:55 +0100

    setDeploymentImage.sh regex

--- setDeploymentImage.sh
+++ setDeploymentImage.sh
@@ -1,22 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # updates deployment files with new image tag names
 SERVICE="$1"	# frontend | corona-backend | country-backend
-TAG_NAME="$2"	# e.d registry.guldner.eu/frontend:20220116-1320
-
-#if [[ -z "$SERVICE" ]]; then
-#	printf "SERVICE is not provided"
-#	exit 1
-#fi
-
-#if [[ -z "$TAG_NAME" ]]; then
-#	printf "TAG_NAME is not provided"
-#	exit 1
-#fi
-
+TAG_NAME="$2"	# e.d registry.guldner.eu/frontend:20220116-132
 DEPLOYMENT_FILE="./cluster/$SERVICE/resources/deployment.yaml"
 
 cat $DEPLOYMENT_FILE
-#sed -i '0,/^\([[:space:]]*image: *\).*/s//\1'$TAG_NAME'/;' $DEPLOYMENT_FILE
-sed -i  "/^\([[:space:]]*image: \).*/s//\1$TAG_NAME/" $DEPLOYMENT_FILE
+sed -i "s/^\(\s*image\s*:\s*\).*/\1$TAG_NAME/" $DEPLOYMENT_FILE
 cat $DEPLOYMENT_FILE
